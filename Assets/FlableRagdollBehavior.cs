8e541060dcd95e633f144ce237273812728b90fc
ouroborose <rose.peng@gatech.edu>
2018-07-31 23:56:18 -0700

    Flables properly ragdoll, launch, score, and disappear.

---
+++ Assets/FlableRagdollBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FlableRagdollBehavior : MonoBehaviour {
+
+    public Rigidbody[] ragdollRigidbodies;
+
+	// Use this for initialization
+	void Start () {
+        ragdollRigidbodies = gameObject.GetComponentsInChildren<Rigidbody>();
+        StartCoroutine(KillFlable(2.0f));
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake ()
+    {
+        
+    }
+
+    void FixedUpdate ()
+    {
+        
+
+        //Debug.DrawRay(ragdollRigidbody.position, ragdollRigidbody.velocity, Color.red, 10.0f);
+    }
+
+    IEnumerator KillFlable(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        for (int i = 0; i < ragdollRigidbodies.Length; i++)
+        {
+            ragdollRigidbodies[i].isKinematic = true;
+        }
+        gameObject.GetComponentInChildren<Rigidbody>().transform.DOScale(0,2.0f);
+
+        yield return new WaitForSeconds(2.0f);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/flableBehavior.cs
+++ Assets/flableBehavior.cs
@@ -3,30 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class flableBehavior : MonoBehaviour {
+public class flableBehavior : MonoBehaviour
+{
 
     public GameObject flableRagdoll;
     public bool hasTipped = false;
-    public int flableScore = 1000;
+    public int flableScore = 100;
+    public float maxSpeed;
+    public GameObject scoreDoober;
+    private Vector3 explosionPos;
+    public float explosionPower;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void Tip ()
+    // Use this for initialization
+    void Start()
     {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponentInParent<TableComponentBehavior>() != null || collision.gameObject.CompareTag("Projectile"))
+        {
+            Debug.Log("This flable has been hit!");
+            explosionPos = collision.gameObject.GetComponentInChildren<Rigidbody>().position;
+            Tip();
+        }
+    }
+
+    public void Tip()
+    {
+
         if (hasTipped == false)
         {
             hasTipped = true;
-            print("Flable has been tipped");
-            Instantiate(flableRagdoll, transform.position, transform.rotation);
+            Debug.Log("Flable has been tipped");
+            GameObject flableRagdollObj = Instantiate(flableRagdoll, transform.position, transform.rotation);
+            Vector3 ragdollVelocity = flableRagdollObj.GetComponentInChildren<Rigidbody>().velocity;
+            //adds an explosion force to the ragdoll flable
+            //flableRagdoll.GetComponentInChildren<Rigidbody>().AddExplosionForce(explosionPower, explosionPos, 3.0f);
+            flableRagdollObj.GetComponentInChildren<Rigidbody>().AddExplosionForce(explosionPower, explosionPos, 3.0f);//AddForce(new Vector3(0f, explosionPower, 0f), ForceMode.VelocityChange);
+            flableRagdollObj.GetComponentInChildren<SkinnedMeshRenderer>().materials[0].color = gameObject.GetComponentInChildren<SkinnedMeshRenderer>().materials[0].color;
+            
+            //ragdollVelocity = Vector3.ClampMagnitude(ragdollVelocity, maxSpeed);
+            Debug.Log("flable velocity clamped to: " + ragdollVelocity);
             Destroy(gameObject);
+
+            ScoreSystem.score += flableScore;
+            ScoreDooberTextBehavior.scoreValue = flableScore;
+            Instantiate(scoreDoober, transform.position, Quaternion.identity);
         }
     }
 }
